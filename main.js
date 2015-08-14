2a68e0e21cb90c5315aeb39d9759dd223710db03
zhangshenxu <zhangshenxu1991@gmail.com>
2015-08-14 14:49:25 +0800

    不监听窗口变化，不git .idea目录

--- main.js
+++ main.js
@@ -50,7 +50,7 @@
 cc.game.onStart = function(){
   cc.view.adjustViewPort(true);
   cc.view.setDesignResolutionSize(750, 1206, cc.ResolutionPolicy.SHOW_ALL);
-  cc.view.resizeWithBrowserSize(true);
+  //cc.view.resizeWithBrowserSize(true);
   //load resources
   cc.LoaderScene.preload(g_resources, function () {
     cc.director.runScene(new StartScene());
